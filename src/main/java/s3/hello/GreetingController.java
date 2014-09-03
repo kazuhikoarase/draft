a07066840aeebcd572e01c236374eec67fbee17c
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2014-09-03 19:52:05 +0900

    commit

---
+++ src/main/java/s3/hello/GreetingController.java
@@ -0,0 +1,24 @@
+package s3.hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+//    @RequestMapping(value="/greeting",method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value="/greeting")
+    public Greeting greeting(
+		@RequestParam(value="name", required=false, defaultValue="World")
+		String name
+    ) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
