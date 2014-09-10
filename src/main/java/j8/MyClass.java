12767fe71afefed7504515de8462836ffaf84bb9
kazuhiko.arase@gmail.com <kazuhiko.arase@gmail.com>
2014-09-10 09:30:32 +0900

    commit

--- src/main/java/j8/MyClass.java
+++
@@ -1,7 +0,0 @@
-package j8;
-
-public class MyClass {
-	public MyClass() {}
-	public int length() { return 0; }
-	public static int length(String s) { return 0; }
-}
--- src/main/java/j8/PropertyGetter.java
+++
@@ -1,6 +0,0 @@
-package j8;
-
-@FunctionalInterface
-public interface PropertyGetter<K, V> {
-	V get(K key) throws Exception;
-}
